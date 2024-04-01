820a63cd370dc9d48cce4d3c18ad17e461c61193
John Shedletsky <113091876+ui0ppk@users.noreply.github.com>
2024-04-01 14:44:35 +0000

    Delete src directory

--- src/ui/shared.ts
+++
@@ -1,8 +0,0 @@
-import { semanticColors } from "@ui/color";
-
-export const cardStyle = {
-    backgroundColor: semanticColors.BACKGROUND_SECONDARY,
-    borderRadius: 16,
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-}
